cd734ce47042652b460e301288b2053e4d289090
Matheus <matheus.goes@netshoes.com>
2017-01-05 09:57:37 -0200

    Added validation for fallback routes

--- src/main/java/br/com/gateway/properties/ZuulGatewayProperties.java
+++ src/main/java/br/com/gateway/properties/ZuulGatewayProperties.java
@@ -1,17 +1,29 @@
 package br.com.gateway.properties;
 
+import static org.springframework.util.StringUtils.isEmpty;
+
 import java.util.Map;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import lombok.Data;
 
 @Data
 @ConfigurationProperties("br.com.gateway")
-public class ZuulGatewayProperties {
+public class ZuulGatewayProperties implements InitializingBean {
 
 	private Map<String, String> customHeaders;
 	private Map<String, FallbackRoute> fallback;
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		this.fallback.forEach((serviceId, route) -> {
+			if (isEmpty(route.getServiceId()) && isEmpty(route.getUrl())) {
+				throw new IllegalArgumentException("Either serviceId or url must be provided for fallback route " + serviceId);
+			}
+		});
+	}
 
 	@Data
 	public static class FallbackRoute {
